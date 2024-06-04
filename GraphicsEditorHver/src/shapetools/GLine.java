d5b584ecb771a268271cef672aa6f3fc263bc94a
BEOMJIN LEE <beomjin754@gmail.com>
2024-06-05 03:50:05 +0900

    [0605] restructure GOval,GLine - remove duplicate function

--- GraphicsEditorHver/src/shapetools/GLine.java
+++ GraphicsEditorHver/src/shapetools/GLine.java
@@ -17,13 +17,6 @@ private Graphics graphics;
 	public GLine clone() {
 		return new GLine();
 	}
-	
-	@Override
-	public void draw(Graphics graphics) {
-		// TODO Auto-generated method stub
-		graphics.drawLine(x1, y1, ox2, oy2);
-	}
-	
 	@Override
 	public void drag(Graphics graphics) {
 		Graphics2D graphics2D = (Graphics2D) graphics;
@@ -34,29 +27,11 @@ private Graphics graphics;
 		graphics2D.draw(shape);
 		shape.setLine(x1, y1, x2, y2);
 		graphics2D.draw(shape);
-		
 	}
 	
 	@Override
 	public void click(Graphics graphics) {
 		// TODO Auto-generated method stub
-		
-	}
-	
-	public void startMove(int x, int y) {
-		super.startMove(graphics, x, y);
-	}
-
-	public void keepMove(Graphics graphics, int x, int y) {
-		super.keepMove(graphics, x, y);
-		
-		Graphics2D graphics2D = (Graphics2D) graphics;
-		graphics2D.setXORMode(graphics2D.getBackground());
-		Line2D.Float shape = (Line2D.Float) this.shape;
-		
-		graphics2D.draw(shape);
-		shape.setLine(shape.getX1()+x2-ox2,shape.getY1()+y2-oy2,shape.getX2()+x2-ox2,shape.getY2()+y2-oy2);
-		graphics2D.draw(shape);
 		
 	}
 	
--- GraphicsEditorHver/src/shapetools/GOval.java
+++ GraphicsEditorHver/src/shapetools/GOval.java
@@ -3,6 +3,7 @@ package shapetools;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.RectangularShape;
 
 public class GOval extends GShape {
 
@@ -17,17 +18,11 @@ public class GOval extends GShape {
 	}
 
 	@Override
-	public void draw(Graphics graphics) {
-		// draw new shape
-		graphics.drawOval(x1, y1, x2 - x1, y2 - y1);
-	}
-
-	@Override
 	public void drag(Graphics graphics) {
 		Graphics2D graphics2D = (Graphics2D) graphics;
 		graphics2D.setXORMode(graphics2D.getBackground());
 		// erase old shape
-		Ellipse2D.Float shape = (Ellipse2D.Float) this.shape;
+		RectangularShape shape = (RectangularShape) this.shape;
 		shape.setFrame(x1, y1, ox2 - x1, oy2 - y1);
 		graphics2D.draw(shape);
 		shape.setFrame(x1, y1, x2 - x1, y2 - y1);
@@ -37,25 +32,6 @@ public class GOval extends GShape {
 	@Override
 	public void click(Graphics graphics) {
 		// TODO Auto-generated method stub
-
-	}
-
-	public void startMove(int x, int y) {
-		super.startMove(graphics, x, y);
-	}
-	
-	public void keepMove(Graphics graphics, int x, int y) {
-		super.keepMove(graphics, x, y);
-
-		Graphics2D graphics2D = (Graphics2D) graphics;
-		graphics2D.setXORMode(graphics2D.getBackground());
-		Ellipse2D.Float shape = (Ellipse2D.Float) this.shape;
-
-		// remove rectangle
-		graphics2D.draw(shape);
-		// redraw
-		shape.setFrame(shape.getX() + x2 - ox2, shape.getY() + y2 - oy2, shape.getWidth(), shape.getHeight());
-		graphics2D.draw(shape);
 
 	}
 
